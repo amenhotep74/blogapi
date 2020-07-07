74ad60f1252edc91ab5ac2700a8b59a2b0ed2d69
Bradley Lewis <Blewie144@gmail.com>
2020-07-07 13:47:11 +0930

    Added get posts on home

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
 
 const PostItem = ({
   auth,
@@ -9,9 +10,13 @@ const PostItem = ({
   return (
     <div className='border p-1 m-3'>
       <span class='text-muted'>
-        Author Name: {name} / / Date Posted: {date} / AuthorID: {user}
+        Author Name: {name} / / Date Posted:{' '}
+        <Moment format='YYYY/MM/DD'>{date}</Moment> / AuthorID: {user}
       </span>
       <p>{text}</p>
+      <button className='btn btn-success mr-1'>Read More</button>
+      <button className='btn btn-primary mr-1'>Edit</button>
+      <button className='btn btn-danger'>Delete Post</button>
     </div>
   );
 };
